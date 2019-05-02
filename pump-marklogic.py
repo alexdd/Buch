df11c74e66cfe88726d457ba2c82d1b1ef79e203
Alex Düsel <tekturcms@gmail.com>
2019-05-02 23:29:33 +0200

    synchronized Tektur instance with GIT

--- pump-marklogic.py
+++ pump-marklogic.py
@@ -1,6 +1,8 @@
 import os
 from subprocess import call
 
+# this script pumps the topics into MarkLogic
+
 book_dir = "tektur_6478ddd6-a447-4a5d-a467-1d3820bf7049"
 pathes = []
 
@@ -8,7 +10,6 @@ for f in os.listdir(book_dir):
 	if os.path.isdir(os.path.join(book_dir,f)):
 		pathes.append(os.path.join(book_dir,f,'resource.xml'))
 pathes.append(os.path.join(book_dir,'resource.xml'))
-
 
 for p in pathes:
 	
@@ -20,19 +21,17 @@ for p in pathes:
 		'-username',
 		'alex',
 		'-password',
-		'anoma66',
+		'alex',
 		'-host',
 		'localhost',
 		'-input_file_path',
 		p,
-		'-input_file_type', 
-		'aggregates',
 		'-output_collections',
 		'/topics',
 		'-output_uri_prefix',
-		'/topic/',
-		'-output_uri_suffix',
-		'.xml']
+		'/topic',
+		 '-output_uri_replace',
+		 "/C:/Buch,''"]
 	try:
 		call(args)
 	except:
